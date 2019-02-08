01529ca0fda297b30223fea1a7a2c1edd39f91a3
liumingchun <psymingchun@gmail.com>
2019-02-08 09:49:30 +0800

    Psychology;Finance,Business and Economics Professor

--- RateMypro_psychology_finance.py
+++ RateMypro_psychology_finance.py
@@ -1,32 +1,41 @@
-#从数据集文件夹3-4中提取心理学教授信息，并将教授信息补全.文件夹3提取到第202060文件.最后筛选了4903条数据
-#从数据集文件夹3-4提取金融专业教授信息，金融学院 Finance 文件夹3提取到64857。 最后筛选出3211条数据
-#
+
+#++++++++++金融类教授数据集++++++++++#
+#提取说明：以Finance，Business和Economics为关键词筛选
+
+#++++++++++心理学教授数据集++++++++++#
+#提取时间：2019年2月7日
+# 1.【Extract_RateMyProfessor1】提取有效数据9436个
+# 2.【Extract_RateMyProfessor2】提取有效数据21625个
+# 3.【Extract_RateMyProfessor3】提取有效数据20897个
+# 4.【Extract_RateMyProfessor4】提取有效数据5545个
+# 心理学教授有效数据共计36606
+
 import pandas as pd
-import os
-src='K:\RateMyProfessor3'
+import shutil
+
+src = r'C:\Users\Administrator\Desktop\Extract_RateMyProfessor1'
+save_path = r'C:\Users\Administrator\Desktop\psy1'
+save_path1 = r'C:\Users\Administrator\Desktop\Finance1'
+save_path2 = r'C:\Users\Administrator\Desktop\Economics1'
+
 filelist = os.listdir(src)
 
-
-num = 193
+num = 1
 for i in range(1,len(filelist)):
     newsrc =src +'\\'+ filelist[i]
     df = pd.read_csv(newsrc)
     try:
-        if 'Finance' in df['department_name'][0]:
+        if 'Psychology' in df['department_name'][0]:
             print(df['department_name'][0],'第'+str(num)+'文件','文件地址：'+ newsrc)
+            shutil.copy(newsrc,save_path)
             num += 1
-            # 填充信息
-            df.professor_name.ffill(inplace=True)
-            df.school_name.ffill(inplace=True)
-            df.department_name.ffill(inplace=True)
-            df.local_name.ffill(inplace=True)
-            df.state_name.ffill(inplace=True)
-            df.star_rating.ffill(inplace=True)
-            df.take_again.ffill(inplace=True)
-            df.diff_index.ffill(inplace=True)
-            df.tag_professor.ffill(inplace=True)
-            df.num_student.ffill(inplace=True)
-            df.to_csv('pro-'+str(num)+'.csv',index=False)
-
+        # elif 'Finance1' in df['department_name'][0]:
+        #     print(df['department_name'][0], '第' + str(num) + '文件', '文件地址：' + newsrc)
+        #     shutil.copy(newsrc, save_path1)
+        #     num += 1
+        # elif 'Economics' in df['department_name'][0]:
+        #     print(df['department_name'][0], '第' + str(num) + '文件', '文件地址：' + newsrc)
+        #     shutil.copy(newsrc, save_path2)
+        #     num += 1
     except:
         pass
